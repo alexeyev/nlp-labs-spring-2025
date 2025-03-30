8a88920ccf3bb2516a07c43bd63bc16be49323fd
Anton Alekseev <anton.m.alexeyev@gmail.com>
2025-03-31 01:32:08 +0300

    [ENH] One more teaser for Topic 6

---
+++ 06_sequence_tagging/README.md
@@ -0,0 +1,15 @@
+## Заголовки постов, в которых пользователи Reddit просят посоветовать музыку
+
+Это хороший пример, почему NER -- это далеко не всегда очень уж просто.
+
+Данные из работы c конференции EACL2023:
+
+```
+@InProceedings{Epure2023,
+  title={A Human Subject Study of Named Entity Recognition (NER) in Conversational Music Recommendation Queries},
+  author={Epure, Elena and Hennequin, Romain},
+  booktitle={Proceedings of the 17th Conference of the European Chapter of the Association for Computational Linguistics (EACL)},
+  month={May},
+  year={2023}
+}
+```
---
+++ 06_sequence_tagging/prepare_music_reviews.py
@@ -0,0 +1,23 @@
+"""
+Надо скачать и распаковать вот это:
+ https://mcauleylab.ucsd.edu/public_datasets/data/amazon_v2/categoryFiles/Digital_Music.json.gz
+"""
+
+import json
+
+from nltk.tokenize import sent_tokenize, word_tokenize
+from tqdm import tqdm
+
+if __name__ == "__main__":
+
+    reviews = [json.loads(line).get("reviewText", None)
+               for line in open("Digital_Music.json", "r", encoding="utf-8")]
+
+    with open("tmp-train.txt", "w", encoding="utf-8") as wf:
+        for review in tqdm(reviews, "reviews"):
+            if review is None or not review.strip():
+                continue
+            sentences = sent_tokenize(review, language="english")
+            for sentence in sentences:
+                toks = [w for w in word_tokenize(sentence.lower()) if str.isalpha(w)]
+                wf.write(" ".join(toks) + "\n")
