9d24c76f3fe0d6053b07884666109654e6447645
Anton Alekseev <anton.m.alexeyev@gmail.com>
2025-03-31 02:16:47 +0300

    [ENH] Word vectors training

---
+++ 06_sequence_tagging/custom_fasttext.py
@@ -0,0 +1,34 @@
+import multiprocessing
+import os
+
+import fasttext
+from gensim.models.fasttext import FastTextKeyedVectors, load_facebook_vectors
+
+unsupervised_default = {
+    'model': "skipgram",
+    'dim': 100,
+    'ws': 5,
+    'epoch': 5,
+    'minCount': 10,
+    'minn': 3,
+    'maxn': 6,
+    'neg': 5,
+    'wordNgrams': 1,
+    'loss': "ns",
+    'thread': multiprocessing.cpu_count() - 1,
+    'lrUpdateRate': 100000,
+    'verbose': 2,
+}
+
+if any([f.startswith("fasttext-custom") for f in os.listdir()]):
+    ft: FastTextKeyedVectors = load_facebook_vectors("fasttext-custom.bin")
+    print("Please:", ft.most_similar(positive=["please"]))
+    print("Other:", ft.most_similar(positive=["other"]))
+    print("Aphex:", ft.most_similar(positive=["aphex"]))
+    print("Album:", ft.most_similar(positive=["album"]))
+    print("Song:", ft.most_similar(positive=["song"]))
+    print("Band:", ft.most_similar(positive=["band"]))
+    quit()
+
+model = fasttext.train_unsupervised("tmp-train.txt", **unsupervised_default)
+model.save_model("fasttext-custom.bin")
